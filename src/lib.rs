1079b976d051d708fca2cc3a72825ebc2eba1c3a
killian <killiancharrier8@gmail.com>
2022-08-22 15:56:27 +0200

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use windows::{
     core::*, Win32::Foundation::*, Win32::Graphics::Gdi::*, Win32::UI::Controls::*,
-    Win32::UI::Shell::DefSubclassProc, Win32::UI::WindowsAndMessaging::*,
+    Win32::UI::Shell::*, Win32::UI::WindowsAndMessaging::*,
 };
 
 pub fn create_window<T: Into<PCWSTR>>(
@@ -8,6 +8,7 @@ pub fn create_window<T: Into<PCWSTR>>(
     name: &str,
     [width, height]: [i32; 2],
     instance: HINSTANCE,
+    lpparam: *const core::ffi::c_void,
 ) -> HWND {
     unsafe {
         CreateWindowExW(
@@ -22,23 +23,28 @@ pub fn create_window<T: Into<PCWSTR>>(
             None,
             None,
             instance,
-            std::ptr::null(),
+            lpparam,
         )
     }
 }
 
 pub fn create_control<T: Into<PCWSTR>>(
     class: T,
+    style: Option<u32>,
     [width, height]: [i32; 2],
     parent: HWND,
     instance: HINSTANCE,
 ) -> HWND {
+    let mut styles = WS_CHILD | WS_VISIBLE;
+    if !style.is_none() {
+        styles = styles | WINDOW_STYLE(style.unwrap());
+    }
     unsafe {
         CreateWindowExW(
             WINDOW_EX_STYLE::default(),
             class,
             PCWSTR::null(),
-            WS_CHILD | WS_VISIBLE,
+            styles,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
             width,
@@ -51,53 +57,36 @@ pub fn create_control<T: Into<PCWSTR>>(
     }
 }
 
-enum State {
+#[derive(Default)]
+pub enum State {
+    #[default]
     Idle,
     Down,
     Hover,
 }
 
-pub struct ButtonData {
-    state: State,
-    index: usize,
-    icons: Vec<[HICON; 2]>,
+#[derive(Default)]
+pub struct Control {
+    pub hwnd: Option<HWND>,
+    pub class: HSTRING,
+    pub style: Option<u32>,
+    pub proc: SUBCLASSPROC,
+    pub x: (f32, i32),
+    pub y: (f32, i32),
+    pub size: [i32; 2],
+    pub data: usize,
 }
-impl ButtonData {
-    pub unsafe fn new(icons_file: Vec<[&str; 2]>, instance: HINSTANCE) -> Result<ButtonData> {
-        Ok(ButtonData {
-            state: State::Idle,
-            index: 0,
-            icons: {
-                let mut icons = Vec::new();
-                for [idle, hover] in icons_file {
-                    let idle_ico = HICON(
-                        LoadImageW(
-                            instance,
-                            PCWSTR::from(&HSTRING::from(idle)),
-                            IMAGE_ICON,
-                            0,
-                            0,
-                            LR_LOADFROMFILE,
-                        )?
-                        .0,
-                    );
-                    let hover_ico = HICON(
-                        LoadImageW(
-                            instance,
-                            PCWSTR::from(&HSTRING::from(hover)),
-                            IMAGE_ICON,
-                            0,
-                            0,
-                            LR_LOADFROMFILE,
-                        )?
-                        .0,
-                    );
-                    icons.push([idle_ico, hover_ico]);
-                }
-                icons
-            },
-        })
-    }
+
+#[derive(Default)]
+pub struct ButtonData {
+    pub state: State,
+    pub index: usize,
+    pub icons: Vec<[HICON; 2]>,
+}
+
+#[derive(Default)]
+pub struct TrackbarData {
+    pub state: State,
 }
 
 pub extern "system" fn buttonproc(
@@ -157,6 +146,97 @@ pub extern "system" fn buttonproc(
             _ => (),
         }
         DefSubclassProc(hwnd, umsg, wparam, lparam)
+    }
+}
+
+pub extern "system" fn trackbarproc(
+    hwnd: HWND,
+    umsg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+    _uidsubclass: usize,
+    dwrefdata: usize,
+) -> LRESULT {
+    unsafe {
+        match umsg as u32 {
+            WM_PAINT => {
+                let mut client_rc = RECT::default();
+                GetClientRect(hwnd, &mut client_rc);
+                InvalidateRect(hwnd, &client_rc, BOOL(0)).ok().unwrap();
+
+                let mut ps = PAINTSTRUCT::default();
+                let hdc = BeginPaint(hwnd, &mut ps);
+
+                let mut thumb = RECT::default();
+                SendMessageW(
+                    hwnd,
+                    TBM_GETTHUMBRECT,
+                    WPARAM(0),
+                    LPARAM((&mut thumb) as *mut _ as isize),
+                );
+
+                let data = dwrefdata as *mut TrackbarData;
+
+                SetDCBrushColor(hdc, 0x00181818);
+                FillRect(hdc, &ps.rcPaint, HBRUSH(GetStockObject(DC_BRUSH).0));
+
+                let old_brush = SelectObject(hdc, CreateSolidBrush(0x005E5E5E));
+                let old_pen = SelectObject(hdc, CreatePen(PS_SOLID, 0, 0x005E5E5E));
+
+                RoundRect(hdc, client_rc.left + 6, 6, client_rc.right - 6, 10, 2, 2);
+
+                match (*data).state {
+                    State::Idle => {
+                        SelectObject(hdc, CreateSolidBrush(0x00FFFFFF));
+                        SelectObject(hdc, CreatePen(PS_SOLID, 0, 0x00FFFFFF));
+                        RoundRect(hdc, client_rc.left + 6, 6, thumb.right, 10, 2, 2);
+                    }
+                    State::Hover => {
+                        SelectObject(hdc, CreateSolidBrush(0x001DB954));
+                        SelectObject(hdc, CreatePen(PS_SOLID, 0, 0x001DB954));
+                        RoundRect(hdc, client_rc.left + 6, 6, thumb.right, 10, 2, 2);
+                        SelectObject(hdc, CreateSolidBrush(0x00FFFFFF));
+                        SelectObject(hdc, CreatePen(PS_SOLID, 0, 0x00FFFFFF));
+                        Ellipse(
+                            hdc,
+                            thumb.left - 2,
+                            thumb.top + 1,
+                            thumb.right + 2,
+                            thumb.bottom - 1,
+                        );
+                    }
+                    _ => (),
+                }
+
+                SelectObject(hdc, old_brush);
+                SelectObject(hdc, old_pen);
+
+                EndPaint(hwnd, &mut ps).ok().unwrap();
+                LRESULT(0)
+            }
+            WM_MOUSEMOVE => {
+                let data = dwrefdata as *mut TrackbarData;
+                match (*data).state {
+                    State::Idle => {
+                        (*data).state = State::Hover;
+                        let mut client_rc = RECT::default();
+                        GetClientRect(hwnd, &mut client_rc);
+                        InvalidateRect(hwnd, &client_rc, BOOL(0)).ok().unwrap();
+                    }
+                    _ => (),
+                }
+                DefSubclassProc(hwnd, umsg, wparam, lparam)
+            }
+            WM_MOUSELEAVE => {
+                let data = dwrefdata as *mut TrackbarData;
+                (*data).state = State::Idle;
+                let mut client_rc = RECT::default();
+                GetClientRect(hwnd, &mut client_rc);
+                InvalidateRect(hwnd, &client_rc, BOOL(0)).ok().unwrap();
+                DefSubclassProc(hwnd, umsg, wparam, lparam)
+            }
+            _ => DefSubclassProc(hwnd, umsg, wparam, lparam),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use windows::{
     core::*, Win32::Foundation::*, Win32::Graphics::Gdi::*,
-    Win32::System::LibraryLoader::GetModuleHandleW, Win32::UI::Shell::SetWindowSubclass,
-    Win32::UI::WindowsAndMessaging::*,
+    Win32::System::LibraryLoader::GetModuleHandleW, Win32::System::WindowsProgramming::*,
+    Win32::UI::Controls::*, Win32::UI::Shell::SetWindowSubclass, Win32::UI::WindowsAndMessaging::*,
 };
 
-use may::{buttonproc, create_control, create_window, hiword, loword, ButtonData};
+use may::{
+    buttonproc, create_control, create_window, hiword, loword, trackbarproc, ButtonData, Control,
+    TrackbarData,
+};
 
 fn main() -> Result<()> {
     unsafe {
-        let instance = GetModuleHandleW(None)?;
-        debug_assert!(instance.0 != 0);
+        let ins = GetModuleHandleW(None)?;
+        debug_assert!(ins.0 != 0);
 
         let window_class = w!("window");
-        let button_class = w!("button");
 
         let wc = WNDCLASSW {
             hCursor: LoadCursorW(None, IDC_ARROW)?,
-            hInstance: instance,
+            hInstance: ins,
             lpszClassName: PCWSTR::from(window_class),
             style: CS_HREDRAW | CS_VREDRAW,
             lpfnWndProc: Some(wndproc),
@@ -26,22 +28,69 @@ fn main() -> Result<()> {
         let atom = RegisterClassW(&wc);
         debug_assert!(atom != 0);
 
-        let hwnd = create_window(window_class, "spotify", [600, 200], instance);
+        let mut controls: Vec<Control> = Vec::new();
 
-        let mut hwnds: Vec<(HWND, [(f32, i32); 2])> = Vec::new();
+        let mut btn1 = ButtonData {
+            icons: vec![[
+                HICON(LoadImageW(ins, w!("icons/play.ico"), IMAGE_ICON, 0, 0, LR_LOADFROMFILE)?.0),
+                HICON(
+                    LoadImageW(
+                        ins,
+                        w!("icons/playh.ico"),
+                        IMAGE_ICON,
+                        0,
+                        0,
+                        LR_LOADFROMFILE,
+                    )?
+                    .0,
+                ),
+            ]],
+            ..Default::default()
+        };
+        controls.push(Control {
+            class: HSTRING::from("Button"),
+            proc: Some(buttonproc),
+            x: (0.5, 0),
+            y: (1.0, -74),
+            size: [34, 34],
+            data: &mut btn1 as *mut _ as usize,
+            ..Default::default()
+        });
 
-        let child = create_control(button_class, [32, 32], hwnd, instance);
-        let mut data = ButtonData::new(vec![["btn.ico", "btn_h.ico"], ["btn2.ico", "btn2_h.ico"]], instance)?;
-        SetWindowSubclass(child, Some(buttonproc), 0, (&mut data) as *mut _ as usize).ok()?;
-        hwnds.push((child, [(0.5, 0), (1.0, -74)]));
+        let mut tbr1 = TrackbarData::default();
+        controls.push(Control {
+            class: HSTRING::from("msctls_trackbar32"),
+            style: Some(TBS_NOTICKS),
+            proc: Some(trackbarproc),
+            x: (0.5, -100),
+            y: (1.0, -24),
+            size: [200, 18],
+            data: &mut tbr1 as *mut _ as usize,
+            ..Default::default()
+        });
 
-        SetPropW(hwnd, w!("hwnds"), HANDLE((&hwnds) as *const _ as isize)).ok()?;
+        let ptr: *const core::ffi::c_void = &mut controls as *mut _ as *const core::ffi::c_void;
+        let _hwnd = create_window(window_class, "spotify", [600, 200], ins, ptr);
+
+        /*let result = AddFontResourceExW(w!("GothamBook.ttf"), FR_PRIVATE, core::ptr::null_mut());
+        debug_assert!(result != 0);
+
+        let mut lg = LOGFONTW::default();
+        lg.lfHeight = -MulDiv(8, GetDeviceCaps(hdc, LOGPIXELSY), 72);
+        for (a, b) in "Gotham".encode_utf16().enumerate() {
+            lg.lfFaceName[a] = b;
+        }
+
+        let hfont = CreateFontIndirectW(&lg as *const _);
+        SetPropW(hwnd, w!("font"), HANDLE(&hfont as *const _ as isize));*/
 
         let mut msg = MSG::default();
 
         while GetMessageA(&mut msg, HWND(0), 0, 0).into() {
             DispatchMessageA(&msg);
         }
+
+        //RemoveFontResourceExW(w!("GothamBook.ttf"), FR_PRIVATE.0, core::ptr::null_mut()).ok()?;
 
         Ok(())
     }
@@ -50,6 +99,23 @@ fn main() -> Result<()> {
 extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
         match msg as u32 {
+            WM_CREATE => {
+                let crt = lparam.0 as *const CREATESTRUCTW;
+                let ins = (*crt).hInstance;
+                let controls = (*crt).lpCreateParams as *mut Vec<Control>;
+                for control in &mut *controls {
+                    let child =
+                        create_control(&control.class, control.style, control.size, hwnd, ins);
+                    SetWindowSubclass(child, control.proc, 0, control.data);
+                    control.hwnd = Some(child);
+                }
+
+                SetPropW(hwnd, w!("controls"), HANDLE(controls as *mut _ as isize))
+                    .ok()
+                    .unwrap();
+
+                LRESULT(0)
+            }
             WM_PAINT => {
                 let mut ps = PAINTSTRUCT::default();
                 let _hdc = BeginPaint(hwnd, &mut ps);
@@ -57,24 +123,64 @@ extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM)
                 EndPaint(hwnd, &mut ps).ok().unwrap();
                 LRESULT(0)
             }
+            WM_ERASEBKGND => {
+                let hdc = HDC(wparam.0 as isize);
+                let mut rc = RECT::default();
+                GetClientRect(hwnd, &mut rc);
+
+                SetDCBrushColor(hdc, 0x00000000);
+                FillRect(
+                    hdc,
+                    &RECT {
+                        left: 0,
+                        top: 0,
+                        right: 240,
+                        bottom: rc.bottom - 92,
+                    },
+                    HBRUSH(GetStockObject(DC_BRUSH).0),
+                );
+
+                SetDCBrushColor(hdc, 0x00525252);
+                FillRect(
+                    hdc,
+                    &RECT {
+                        left: 0,
+                        top: rc.bottom - 92,
+                        right: rc.right,
+                        bottom: rc.bottom - 91,
+                    },
+                    HBRUSH(GetStockObject(DC_BRUSH).0),
+                );
+                SetDCBrushColor(hdc, 0x00181818);
+                FillRect(
+                    hdc,
+                    &RECT {
+                        left: 0,
+                        top: rc.bottom - 91,
+                        right: rc.right,
+                        bottom: rc.bottom,
+                    },
+                    HBRUSH(GetStockObject(DC_BRUSH).0),
+                );
+
+                LRESULT(0)
+            }
             WM_SIZE => {
-                let width = loword(lparam.0 as u32);
-                let height = hiword(lparam.0 as u32);
-                let handle = GetPropW(hwnd, w!("hwnds"));
-                if !handle.is_invalid() {
-                    let data = handle.0 as *const Vec<(HWND, [(f32, i32); 2])>;
-                    let mut hdwp = BeginDeferWindowPos((*data).len() as i32);
-                    for (hwnd, pos) in &*data {
-                        let x = (width as f32 * pos[0].0) as i32 + pos[0].1;
-                        let y = (height as f32 * pos[1].0) as i32 + pos[1].1;
-                        hdwp = DeferWindowPos(hdwp, *hwnd, HWND(0), x, y, 0, 0, SWP_NOSIZE);
-                    }
-                    EndDeferWindowPos(hdwp).ok().unwrap();
+                let width = loword(lparam.0 as u32) as f32;
+                let height = hiword(lparam.0 as u32) as f32;
+
+                let controls = GetPropW(hwnd, w!("controls")).0 as *mut Vec<Control>;
+                let mut hdwp = BeginDeferWindowPos((*controls).len() as i32);
+                for control in &*controls {
+                    let x = (width * control.x.0) as i32 + control.x.1;
+                    let y = (height * control.y.0) as i32 + control.y.1;
+                    hdwp = DeferWindowPos(hdwp, control.hwnd, HWND(0), x, y, 0, 0, SWP_NOSIZE);
                 }
+                EndDeferWindowPos(hdwp).ok().unwrap();
                 LRESULT(0)
             }
             WM_DESTROY => {
-                RemovePropW(hwnd, w!("hwnds")).ok().unwrap();
+                RemovePropW(hwnd, w!("controls")).ok().unwrap();
                 PostQuitMessage(0);
                 LRESULT(0)
             }
